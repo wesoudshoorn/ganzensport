b7803cbaefb07c6f3f04307c5d12a6e8bf6476e8
Anton Kouliavtsev <seoanton@gmail.com>
2020-04-22 16:30:41 +0200

    fix: ESLintError

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
@@ -14,40 +13,109 @@ const IndexPage = () => (
         <ol className="grid gap-6 text-xl">
           <li>
             <h2 className="font-bold text-3xl mb-2">Kies je sport</h2>
-            <p className="text-gray-500">We hebben momenteel alleen nog tennisversie, maar er komen snel meer sporten aan!</p>
+            <p className="text-gray-500">
+              We hebben momenteel alleen nog tennisversie, maar er komen snel
+              meer sporten aan!
+            </p>
           </li>
           <li>
             <h2 className="font-bold text-3xl mb-2">Print het bordspel uit</h2>
-            <p className="text-gray-500">Download de PDF en print het uit. Als vereniging kun je natuurlijk ook printen op A3 en het mooi lamineren.</p>
+            <p className="text-gray-500">
+              Download de PDF en print het uit. Als vereniging kun je natuurlijk
+              ook printen op A3 en het mooi lamineren.
+            </p>
           </li>
           <li>
             <h2 className="font-bold text-3xl mb-2">Dobbel, video, sport!</h2>
-            <p className="text-gray-500">Je hebt alleen zelf een dobbelsteen en een paar pionnen nodig.</p>
+            <p className="text-gray-500">
+              Je hebt alleen zelf een dobbelsteen en een paar pionnen nodig.
+            </p>
           </li>
         </ol>
       </div>
       <div className="">
-        <h1 className="font-bold text-5xl mb-4 leading-tight">Kies je sport!</h1>
+        <h1 className="font-bold text-5xl mb-4 leading-tight">
+          Kies je sport!
+        </h1>
         <ul className="text-3xl font-bold uppercase">
           <li>
-            <Link to="/tennis" className="group flex border-b-4 border-gray-600 py-3 justify-between items-center">Tennis <span className="group-hover:scale-150 transition duration-200 ease-in-out transform">🎾</span></Link>
+            <Link
+              to="/tennis"
+              className="group flex border-b-4 border-gray-600 py-3 justify-between items-center"
+            >
+              Tennis{" "}
+              <span
+                role="img"
+                aria-label=""
+                sclassName="group-hover:scale-150 transition duration-200 ease-in-out transform"
+              >
+                🎾
+              </span>
+            </Link>
           </li>
           <li>
-            <Link to="/tennis" className="group flex border-b-4 border-gray-600 py-3 justify-between items-center">Voetbal <span className="group-hover:scale-150 transition duration-200 ease-in-out transform">⚽️</span></Link>
+            <Link
+              to="/tennis"
+              className="group flex border-b-4 border-gray-600 py-3 justify-between items-center"
+            >
+              Voetbal{" "}
+              <span
+                role="img"
+                aria-label=""
+                sclassName="group-hover:scale-150 transition duration-200 ease-in-out transform"
+              >
+                ⚽️
+              </span>
+            </Link>
           </li>
           <li>
-            <Link to="/tennis" className="group flex border-b-4 border-gray-600 py-3 justify-between items-center">Fitness <span className="group-hover:scale-150 transition duration-200 ease-in-out transform">🏋️‍♀️</span></Link>
+            <Link
+              to="/tennis"
+              className="group flex border-b-4 border-gray-600 py-3 justify-between items-center"
+            >
+              Fitness{" "}
+              <span
+                role="img"
+                aria-label=""
+                sclassName="group-hover:scale-150 transition duration-200 ease-in-out transform"
+              >
+                🏋️‍♀️
+              </span>
+            </Link>
           </li>
           <li>
-            <Link to="/tennis" className="group flex border-b-4 border-gray-600 py-3 justify-between items-center">Volleybal <span className="group-hover:scale-150 transition duration-200 ease-in-out transform">🏐</span></Link>
+            <Link
+              to="/tennis"
+              className="group flex border-b-4 border-gray-600 py-3 justify-between items-center"
+            >
+              Volleybal{" "}
+              <span
+                role="img"
+                aria-label=""
+                sclassName="group-hover:scale-150 transition duration-200 ease-in-out transform"
+              >
+                🏐
+              </span>
+            </Link>
           </li>
           <li>
-            <Link to="/tennis" className="group flex border-b-4 border-gray-600 py-3 justify-between items-center">Boksen <span className="group-hover:scale-150 transition duration-200 ease-in-out transform">🥊</span></Link>
+            <Link
+              to="/tennis"
+              className="group flex border-b-4 border-gray-600 py-3 justify-between items-center"
+            >
+              Boksen{" "}
+              <span
+                role="img"
+                aria-label=""
+                sclassName="group-hover:scale-150 transition duration-200 ease-in-out transform"
+              >
+                🥊
+              </span>
+            </Link>
           </li>
         </ul>
       </div>
     </div>
-
   </Layout>
 )
 
--- src/pages/tennis.js
+++ src/pages/tennis.js
@@ -2,7 +2,6 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const TennisPage = () => (
@@ -13,73 +12,180 @@ const TennisPage = () => (
         <h1 className="font-bold text-5xl mb-4 leading-tight">Oefeningen</h1>
         <ol className="text-xl max-h-screen overflow-auto ">
           <li>
-            <Link href="#" className="flex items-center border-b-2 border-gray-600 py-3"><span className="w-12">1.</span> Naam van de oefening</Link>
+            <Link
+              href="#"
+              className="flex items-center border-b-2 border-gray-600 py-3"
+            >
+              <span className="w-12">1.</span> Naam van de oefening
+            </Link>
           </li>
           <li>
-            <Link href="#" className="flex items-center border-b-2 border-gray-600 py-3"><span className="w-12">2.</span> Naam van de oefening</Link>
+            <Link
+              href="#"
+              className="flex items-center border-b-2 border-gray-600 py-3"
+            >
+              <span className="w-12">2.</span> Naam van de oefening
+            </Link>
           </li>
           <li>
-            <Link href="#" className="flex items-center border-b-2 border-gray-600 py-3"><span className="w-12">3.</span> Naam van de oefening</Link>
+            <Link
+              href="#"
+              className="flex items-center border-b-2 border-gray-600 py-3"
+            >
+              <span className="w-12">3.</span> Naam van de oefening
+            </Link>
           </li>
           <li>
-            <Link href="#" className="flex items-center border-b-2 border-gray-600 py-3"><span className="w-12">4.</span> Naam van de oefening</Link>
+            <Link
+              href="#"
+              className="flex items-center border-b-2 border-gray-600 py-3"
+            >
+              <span className="w-12">4.</span> Naam van de oefening
+            </Link>
           </li>
           <li>
-            <Link href="#" className="flex items-center border-b-2 border-gray-600 py-3"><span className="w-12">5.</span> Naam van de oefening</Link>
+            <Link
+              href="#"
+              className="flex items-center border-b-2 border-gray-600 py-3"
+            >
+              <span className="w-12">5.</span> Naam van de oefening
+            </Link>
           </li>
           <li>
-            <Link href="#" className="flex items-center border-b-2 border-gray-600 py-3"><span className="w-12">1.</span> Naam van de oefening</Link>
+            <Link
+              href="#"
+              className="flex items-center border-b-2 border-gray-600 py-3"
+            >
+              <span className="w-12">1.</span> Naam van de oefening
+            </Link>
           </li>
           <li>
-            <Link href="#" className="flex items-center border-b-2 border-gray-600 py-3"><span className="w-12">2.</span> Naam van de oefening</Link>
+            <Link
+              href="#"
+              className="flex items-center border-b-2 border-gray-600 py-3"
+            >
+              <span className="w-12">2.</span> Naam van de oefening
+            </Link>
           </li>
           <li>
-            <Link href="#" className="flex items-center border-b-2 border-gray-600 py-3"><span className="w-12">3.</span> Naam van de oefening</Link>
+            <Link
+              href="#"
+              className="flex items-center border-b-2 border-gray-600 py-3"
+            >
+              <span className="w-12">3.</span> Naam van de oefening
+            </Link>
           </li>
           <li>
-            <Link href="#" className="flex items-center border-b-2 border-gray-600 py-3"><span className="w-12">4.</span> Naam van de oefening</Link>
+            <Link
+              href="#"
+              className="flex items-center border-b-2 border-gray-600 py-3"
+            >
+              <span className="w-12">4.</span> Naam van de oefening
+            </Link>
           </li>
           <li>
-            <Link href="#" className="flex items-center border-b-2 border-gray-600 py-3"><span className="w-12">5.</span> Naam van de oefening</Link>
+            <Link
+              href="#"
+              className="flex items-center border-b-2 border-gray-600 py-3"
+            >
+              <span className="w-12">5.</span> Naam van de oefening
+            </Link>
           </li>
           <li>
-            <Link href="#" className="flex items-center border-b-2 border-gray-600 py-3"><span className="w-12">1.</span> Naam van de oefening</Link>
+            <Link
+              href="#"
+              className="flex items-center border-b-2 border-gray-600 py-3"
+            >
+              <span className="w-12">1.</span> Naam van de oefening
+            </Link>
           </li>
           <li>
-            <Link href="#" className="flex items-center border-b-2 border-gray-600 py-3"><span className="w-12">2.</span> Naam van de oefening</Link>
+            <Link
+              href="#"
+              className="flex items-center border-b-2 border-gray-600 py-3"
+            >
+              <span className="w-12">2.</span> Naam van de oefening
+            </Link>
           </li>
           <li>
-            <Link href="#" className="flex items-center border-b-2 border-gray-600 py-3"><span className="w-12">3.</span> Naam van de oefening</Link>
+            <Link
+              href="#"
+              className="flex items-center border-b-2 border-gray-600 py-3"
+            >
+              <span className="w-12">3.</span> Naam van de oefening
+            </Link>
           </li>
           <li>
-            <Link href="#" className="flex items-center border-b-2 border-gray-600 py-3"><span className="w-12">4.</span> Naam van de oefening</Link>
+            <Link
+              href="#"
+              className="flex items-center border-b-2 border-gray-600 py-3"
+            >
+              <span className="w-12">4.</span> Naam van de oefening
+            </Link>
           </li>
           <li>
-            <Link href="#" className="flex items-center border-b-2 border-gray-600 py-3"><span className="w-12">5.</span> Naam van de oefening</Link>
+            <Link
+              href="#"
+              className="flex items-center border-b-2 border-gray-600 py-3"
+            >
+              <span className="w-12">5.</span> Naam van de oefening
+            </Link>
           </li>
           <li>
-            <Link href="#" className="flex items-center border-b-2 border-gray-600 py-3"><span className="w-12">1.</span> Naam van de oefening</Link>
+            <Link
+              href="#"
+              className="flex items-center border-b-2 border-gray-600 py-3"
+            >
+              <span className="w-12">1.</span> Naam van de oefening
+            </Link>
           </li>
           <li>
-            <Link href="#" className="flex items-center border-b-2 border-gray-600 py-3"><span className="w-12">2.</span> Naam van de oefening</Link>
+            <Link
+              href="#"
+              className="flex items-center border-b-2 border-gray-600 py-3"
+            >
+              <span className="w-12">2.</span> Naam van de oefening
+            </Link>
           </li>
           <li>
-            <Link href="#" className="flex items-center border-b-2 border-gray-600 py-3"><span className="w-12">3.</span> Naam van de oefening</Link>
+            <Link
+              href="#"
+              className="flex items-center border-b-2 border-gray-600 py-3"
+            >
+              <span className="w-12">3.</span> Naam van de oefening
+            </Link>
           </li>
           <li>
-            <Link href="#" className="flex items-center border-b-2 border-gray-600 py-3"><span className="w-12">4.</span> Naam van de oefening</Link>
+            <Link
+              href="#"
+              className="flex items-center border-b-2 border-gray-600 py-3"
+            >
+              <span className="w-12">4.</span> Naam van de oefening
+            </Link>
           </li>
           <li>
-            <Link href="#" className="flex items-center border-b-2 border-gray-600 py-3"><span className="w-12">5.</span> Naam van de oefening</Link>
+            <Link
+              href="#"
+              className="flex items-center border-b-2 border-gray-600 py-3"
+            >
+              <span className="w-12">5.</span> Naam van de oefening
+            </Link>
           </li>
         </ol>
       </div>
       <div className="col-span-2">
         <h1 className="font-bold text-5xl mb-4 leading-tight">Oefening 21</h1>
-        <iframe width="704" height="396" src="https://www.youtube-nocookie.com/embed/vD_YDv2ymno" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe
+          title="Video Player"
+          width="704"
+          height="396"
+          src="https://www.youtube-nocookie.com/embed/vD_YDv2ymno"
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
       </div>
     </div>
-
   </Layout>
 )
 
